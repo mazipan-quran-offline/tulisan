1990b7f69d346be6051f930232453c0ff86e4de0
Irfan Maulana <mazipanneh@gmail.com>
2021-07-27 10:40:06 +0700

    Update html.js

--- src/html.js
+++ src/html.js
@@ -1,21 +1,7 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
 
-const adsWrapper = {
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  marginBottom: '2rem',
-};
-
 export default (props) => {
-  React.useEffect(() => {
-    setTimeout(() => {
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    }, 1000);
-  }, []);
-
   return (
     <html {...props.htmlAttributes}>
       <head>
@@ -30,22 +16,6 @@ export default (props) => {
         <div key="body" dangerouslySetInnerHTML={{ __html: props.body }} id="___gatsby" />
 
         {props.postBodyComponents}
-
-        <div className="adswrapper" style={adsWrapper}>
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-
-          <ins
-            className="adsbygoogle"
-            style={{
-              display: 'block',
-              background: '#fff',
-            }}
-						data-ad-client="ca-pub-5442972248172818"
-						data-ad-slot="4265678371"
-						data-ad-format="auto"
-						data-full-width-responsive="true"
-          ></ins>
-        </div>
       </body>
     </html>
   );
