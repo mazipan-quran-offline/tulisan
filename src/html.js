86d3008979583bea2283058abf7e6702ffff3969
Irfan Maulana <mazipanneh@gmail.com>
2021-07-27 10:59:02 +0700

    Update html.js

--- src/html.js
+++ src/html.js
@@ -9,6 +9,9 @@ export default (props) => {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         {props.headComponents}
+        <style>
+          .table-wrapper { width: 100%; overflow: auto; }
+        </style>
       </head>
       <body {...props.bodyAttributes} className="light">
         {props.preBodyComponents}
