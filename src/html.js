e3f8a41d4a46b5b2f3b0156aa8e0948331a76663
Irfan Maulana <mazipanneh@gmail.com>
2023-07-31 09:15:48 +0700

    chore: add adsense

--- src/html.js
+++ src/html.js
@@ -9,6 +9,8 @@ export default (props) => {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         {props.headComponents}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5442972248172818"
+     crossorigin="anonymous"></script>
       </head>
       <body {...props.bodyAttributes} className="light">
         {props.preBodyComponents}
