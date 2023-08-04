ec50f94a7f8fae9a2d2e12b43baa291f864bb5fd
mazipan <mazipan@users.noreply.github.com>
2023-08-04 13:15:43 +0000

    deploy: bbb889acb78bd039fbf2214ac7e09fea7733b85f

--- component---src-pages-404-js-c6d65243de324d5d3dab.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktulisan=self.webpackChunktulisan||[]).push([[883],{2492:function(t,e,a){a.r(e);var n=a(959),l=a(5324),s=a(910);e.default=t=>{let{data:e,location:a}=t;const u=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:u},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-c6d65243de324d5d3dab.js.map
